4d8b39327ae594b9969a8621229ff7d8959ec118
janakutty <35295069+janakutty@users.noreply.github.com>
2018-01-17 16:39:36 +0530

    POSITIVE.C
    CHECK WHETHER THE GIVEN NUMBER IS POSITIVE OR NEGATIVE OR ZERO

---
+++ POSITIVE.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter any number:");
+scanf("%d",&num);
+if(num>0)
+{
+printf("number is positive");
+}
+if(num<0)
+{
+printf("number is negative");
+}
+if(num==0)
+{
+printf("number is zero");
+}
+return 0;
+}
+
