be8c254fa99aea053de2cb16e620654b760dbda6
janakutty <35295069+janakutty@users.noreply.github.com>
2018-01-31 16:01:40 +0530

    POWER.C
    TO CALCULATE POWER

---
+++ POWER.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int pow,num,i=1;
+long int sum=1;
+printf("\n enter a number");
+scanf("%d",&num);
+printf("\n power of a number");
+scanf("%d",&pow);
+while(i<=power)
+{
+sum=sum*num;
+i++;
+}
+printf("\n %d to the power is %d:%1d,num,sum);
+return 0;
+}
