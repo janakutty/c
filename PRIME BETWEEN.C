0bcb2bd36779057e9f7fce2380bd2e68f0958fe1
janakutty <35295069+janakutty@users.noreply.github.com>
2018-02-02 16:24:04 +0530

    PRIME BETWEEN.C
    TO DISPLAY PRIME NUMBER BETWEEN TWO INTERVALS

---
+++ PRIME BETWEEN.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int i,upper,lower;
+printf("enter the lower limit:");
+scanf("%d",&lower);
+printf("enter the upper limit:");
+scanf("%d",&upper);
+printf("prime number list is:");
+for(i=lower+1;i<upper;i++)
+{
+prime=1;
+if(i=2;i<=n/2;i++)
+{
+prime=0;
+break;
+}
+}
+if(prime)
+printf("%d",&i);
+return 0;
+}
