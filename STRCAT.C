366b1e9f2123b29c6e3643a000eac9d2f31cb621
janakutty <35295069+janakutty@users.noreply.github.com>
2018-02-21 16:52:06 +0530

    Create STRCAT.C

---
+++ STRCAT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+char s,j;
+printf("\nEnter the first string");
+scanf("%s",&s);
+printf("\nEnter the second string");
+scanf("%s",&j);
+printf("\nThe concatenate of two string is %s%s",s,j);
+return 0;
+}
