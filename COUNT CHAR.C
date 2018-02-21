60c1774cad8439aa1a9023ef14dd63bf5af81880
janakutty <35295069+janakutty@users.noreply.github.com>
2018-02-21 15:43:04 +0530

    Create COUNT CHAR.C

---
+++ COUNT CHAR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int count_char=0;
+char str[20];
+printf("enter the string:");
+gets(str);
+for(i=0;str[i]=!null;i++)
+{
+count_char++;
+}
+printf("number of characters in string:%d",count_char);
+return 0;
+}
