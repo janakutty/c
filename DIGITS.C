b1838e355c9f78b1b65b8f55b8802198032ce7e4
janakutty <35295069+janakutty@users.noreply.github.com>
2018-02-23 16:07:32 +0530

    Create DIGITS.C

---
+++ DIGITS.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,count=0;
+printf("enter a number:");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("the number of digits are:%d",count);
+return 0;
+}
