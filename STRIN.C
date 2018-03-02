424d32d3ecfe8135414a1785c98ed433b8642a7d
janakutty <35295069+janakutty@users.noreply.github.com>
2018-03-02 15:54:03 +0530

    Rename STRING.C to STRIN.C

---
+++ STRIN.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i;
+char k[1000];
+printf("enter the character:");
+scanf("%c",&k[1000]);
+printf("enter the first k letter:");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+printf("%s",k[i]);
+}
+return 0;
+}
