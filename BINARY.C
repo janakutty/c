e5440883952ad9c8ab7a2e34f94304b0dfeabd5a
janakutty <35295069+janakutty@users.noreply.github.com>
2018-03-02 16:10:30 +0530

    Create BINARY.C

---
+++ BINARY.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,m;
+char a[10];
+scanf("%s",a);
+for(i=0;i<=m;i++)
+{
+if(a[i]=='0' || a[i]=='1')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
+return 0;
+}
