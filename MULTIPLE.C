7514efa377c50abbe7b25a8f1bf3a81704854268
janakutty <35295069+janakutty@users.noreply.github.com>
2018-03-05 16:06:17 +0530

    Create MULTIPLE.C

---
+++ MULTIPLE.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	printf("enter the value:");
+	scanf("%d",&a);
+	if(a%7==0)
+	{
+		printf("multiple of seven:");
+	}
+	else
+	printf("no mulitiple of seven:");
+return 0;
+}
