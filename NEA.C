81f4c06f49389535593dea4d2e1299dd0b3a1b9c
janakutty <35295069+janakutty@users.noreply.github.com>
2018-03-07 16:32:06 +0530

    Rename NEAR.C to NEA.C

---
+++ NEA.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	printf("enter the value:");
+	scanf("%d",&a);
+	if(a%13==0)
+	{
+		printf("multiple of 13:");
+	}
+else
+printf("no multiple of 13:");
+return 0;
+}
